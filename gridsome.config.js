60db473b00fd88c8dfa79ec541f464099a11c676
Ben Sheedy <ben.sheedy@digitalrisks.co.uk>
2019-03-13 11:10:33 +0000

    add tailwind

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,28 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here requires a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const purgecss = require("@fullhuman/postcss-purgecss");
+const tailwind = require("tailwindcss");
+
+const postcssPlugins = [tailwind("./tailwind.js")];
+
+if (process.env.NODE_ENV === "production") postcssPlugins.push(purgecss());
+
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "Gridsome",
+  plugins: [],
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: postcssPlugins
+      }
+    }
+  }
+};
+
+// module.exports = {
+
+// };
